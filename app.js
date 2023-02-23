7b5d0f822231ba4bb977fa654c2a496874129213
Claude Ishimwe <dpqb12haikuo@gmail.com>
2023-02-23 15:31:23 +0200

    Add some css to make it look good and make the template a little bit functionable to add the new item to the list of todos

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser")
 
 const app = express();
-let item = '';
+let items = [];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get('/', function(req, res){
     // res.send("Hello World Let us check the problem");;
@@ -15,14 +16,15 @@ app.get('/', function(req, res){
    let day = "";
    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     day =today.toLocaleString("en-US", options);
-   res.render('index', {kindOfDay : day, newListItem : item});
+   res.render('index', {kindOfDay : day, newListItems : items});
 });
 app.post("/", function(req, res){
-     item = req.body.newItem;
+    let item = req.body.newItem;
+    items.push(item);
     res.redirect("/");
 
 })
 app.listen(5000, function(){
-    console.log("The Server was started at port 5000 and the template is okay.");
+    console.log("The Server was started at port 5000 and the template is good.");
 
 })
